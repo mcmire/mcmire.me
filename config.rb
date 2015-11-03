19ba7b185fc9d0efb139e12127c82b019b6144fd
Elliot Winkler <elliot.winkler@gmail.com>
2015-11-02 23:10:33 -0700

    Clean the config file a bit

--- config.rb
+++ config.rb
@@ -1,9 +1,3 @@
-###
-# Blog settings
-###
-
-# Time.zone = "UTC"
-
 # Make it so that any HTML files inside of subdirectories that hold files for a
 # blog post are not considered as "blog sources"
 Middleman::Blog::BlogTemplateProcessor.class_eval do
@@ -18,80 +12,13 @@ Middleman::Blog::BlogTemplateProcessor.class_eval do
 end
 
 activate :blog do |blog|
-  # This will add a prefix to all links, template references and source paths
-  # blog.prefix = "blog"
-
-  # blog.permalink = "{year}/{month}/{day}/{title}.html"
-  # Matcher for blog source files
   blog.sources = "blog/{year}/{month}/{day}/{title}.html"
-  # blog.taglink = "tags/{tag}.html"
   blog.layout = "article_layout"
-  # blog.summary_separator = /(READMORE)/
-  # blog.summary_length = 250
-  # blog.year_link = "{year}.html"
-  # blog.month_link = "{year}/{month}.html"
-  # blog.day_link = "{year}/{month}/{day}.html"
   blog.default_extension = ".md"
 
-  # blog.tag_template = "tag.html"
-  # blog.calendar_template = "calendar.html"
-
-  # Enable pagination
-  # blog.paginate = true
-  # blog.per_page = 10
-  # blog.page_link = "page/{num}"
-
   page "feed.xml", layout: false
-
   page "sample-post.html", layout: :article_layout
 end
-
-###
-# Compass
-###
-
-# Change Compass configuration
-# compass_config do |config|
-#   config.output_style = :compact
-# end
-
-###
-# Page options, layouts, aliases and proxies
-###
-
-# Per-page layout changes:
-#
-# With no layout
-# page "/path/to/file.html", layout: false
-#
-# With alternative layout
-# page "/path/to/file.html", layout: :otherlayout
-#
-# A path which all have the same layout
-# with_layout :admin do
-#   page "/admin/*"
-# end
-
-# Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
-# proxy "/this-page-has-no-template.html", "/template-file.html", locals: {
-#  which_fake_page: "Rendering a fake page with a local variable" }
-
-###
-# Helpers
-###
-
-# Automatic image dimensions on image_tag helper
-# activate :automatic_image_sizes
-
-# Reload the browser automatically whenever files change
-# activate :livereload
-
-# Methods defined in the helpers block are available in templates
-# helpers do
-#   def some_helper
-#     "Helping"
-#   end
-# end
 
 helpers do
   def page_title
@@ -126,7 +53,6 @@ activate :directory_indexes
 set :markdown_engine, :kramdown
 set :markdown, input: "GFM", enable_coderay: false, hard_wrap: false
 
-# Build-specific configuration
 configure :build do
   # For example, change the Compass output style for deployment
   # activate :minify_css
